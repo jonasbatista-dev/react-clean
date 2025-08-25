0a45366e6089536e1f0cfa03bf1ac7911a473537
jonasbatista-dev <jonassilvat.i@hotmail.com>
2025-08-25 00:27:15 -0300

    feat: ensure RemoteEuthntication calls httpPostClient with correct body

--- src/data/usecases/authentication/remote-authenticaion.ts
+++ src/data/usecases/authentication/remote-authenticaion.ts
@@ -1,12 +1,13 @@
 import { HttpPostClient } from 'data/protocols/http/http-post-client';
+import { AuthenticationParans } from 'domain/usercases/authentication';
 
 export class RemoteAuthentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient
   ) {}
-  async auth(): Promise<void> {
-    await this.httpPostClient.post({ url: this.url });
+  async auth(params: AuthenticationParans): Promise<void> {
+    await this.httpPostClient.post({ url: this.url, body: params });
     return Promise.resolve();
   }
 }
